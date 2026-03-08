13c4fed4b9fcf71f27185fdcf46559caf40697d0
Sofia Salcedo <sg1698906@gmail.com>
2026-03-08 16:19:39 -0500

    clase controller

--- src/auth/user/user.controller.ts
+++ src/auth/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
-import {UserService} from './user.service';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
